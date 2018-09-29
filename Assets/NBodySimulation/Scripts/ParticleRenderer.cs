9f3ae53b9af49d2283e49315788ce302c7ade9bc
Kodai - HomeWorkstation <kodai100@gmail.com>
2018-09-29 17:36:19 +0900

    add NBodySimulation

---
+++ Assets/NBodySimulation/Scripts/ParticleRenderer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class ParticleRenderer : MonoBehaviour {
+
+    [SerializeField] GameObject simObject;
+    
+    [SerializeField] Material particleRenderMat;
+
+    IParticleRenderable simScript;
+
+    private void Start()
+    {
+        simScript = simObject.GetComponent<IParticleRenderable>();
+        
+    }
+
+    private void OnRenderObject()
+    {
+
+        particleRenderMat.SetPass(0);
+        particleRenderMat.SetBuffer("_Particles", simScript.GetParticleBuffer());
+
+        Graphics.DrawProcedural(MeshTopology.Points, simScript.GetParticleNum());
+
+
+    }
+}
